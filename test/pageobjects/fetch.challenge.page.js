62bcf574d47bf96df005c751deac00748e9859fd
nateP82 <npeck@madisoncollege.edu>
2023-10-17 14:08:16 -0500

    updating test to enter all coins

--- test/pageobjects/fetch.challenge.page.js
+++ test/pageobjects/fetch.challenge.page.js
@@ -16,9 +16,84 @@ class FetchChallengePage {
     get gridInput2() {
         return $('#left_1');
     }
+    // Left grid input 3
+    get gridInput3() {
+        return $('#left_2');
+    }
+    // Left grid input 4
+    get gridInput4() {
+        return $('#left_3');
+    }
+    // Left grid input 5
+    get gridInput5() {
+        return $('#left_4');
+    }
+
+    // Right grid input 1
+    get gridRightInput1() {
+        return $('#right_0');
+    }
+    // Right grid input 2
+    get gridRightInput2() {
+        return $('#right_1');
+    }
+    // Right grid input 3
+    get gridRightInput3() {
+        return $('#right_2');
+    }
+    // Right grid input 4
+    get gridRightInput4() {
+        return $('#right_3');
+    }
+    // Right grid input 5
+    get gridRightInput5() {
+        return $('#right_4');
+    }
 
     get coin0() {
         return $('#coin_0');
+    }
+
+    // weight button selector
+    get weighButton() {
+        return $("#weigh");
+    }
+
+    /**
+     * function to enter even numbers into the left grid
+     */
+    async enterEvenCoins(gridInput1, gridInput2, gridInput3, gridInput4, gridInput5) {
+        await this.gridInput1.click();
+        await this.gridInput1.setValue(gridInput1);
+        await this.gridInput2.click();
+        await this.gridInput2.setValue(gridInput2);
+        await this.gridInput3.click();
+        await this.gridInput3.setValue(gridInput3);
+        await this.gridInput4.click()
+        await this.gridInput4.setValue(gridInput4);
+        await this.gridInput5.click();
+        await this.gridInput5.setValue(gridInput5);
+    }
+
+    /**
+     * function to enter odd numbers into the right grid
+     */
+    async enterOddCoins(gridRightInput1, gridRightInput2, gridRightInput3, gridRightInput4) {
+        await this.gridRightInput1.click();
+        await this.gridRightInput1.setValue(gridRightInput1);
+        await this.gridRightInput2.click();
+        await this.gridRightInput2.setValue(gridRightInput2);
+        await this.gridRightInput3.click();
+        await this.gridRightInput3.setValue(gridRightInput3);
+        await this.gridRightInput4.click();
+        await this.gridRightInput4.setValue(gridRightInput4);
+    }
+    /**
+     * function to click the Weigh button
+     */
+    async clickWeigh() {
+        await this.weighButton.waitForClickable();
+        await this.weighButton.click();
     }
 
 }
--- test/specs/fetch.loadPage.js
+++ test/specs/fetch.loadPage.js
@@ -1,13 +1,22 @@
 const { expect } = require('@wdio/globals')
 const FetchChallengePage = require('../pageobjects/fetch.challenge.page');
 
+
 describe('Load fetch challenge page', () => {
 
     it('Load the fetch rewards challenge page', async () => {
-
-        await browser.url('http://sdetchallenge.fetch.com/')
+        // Load the challenge page
+        await browser.url('http://sdetchallenge.fetch.com/');
         await expect(FetchChallengePage.coin0).toBeClickable();
+
+        // Verify that the grid exists and is clickable
         await expect(FetchChallengePage.gridInput1).toBeClickable();
+        await FetchChallengePage.gridInput1.click();
+
+        // Enter in the coins to weigh them
+        await FetchChallengePage.enterEvenCoins(0,2,4,6,8);
+        await FetchChallengePage.enterOddCoins(1,3,5,7);
+        await FetchChallengePage.clickWeigh();
     })
 
 })
